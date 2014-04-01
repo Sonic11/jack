4745f3e2e4c608efe3037dc4ad1378834c62b3dd
Arturo <arturo@iMac-de-labdis12.local>
2014-04-01 13:42:02 -0600

    subida

---
+++ assets/js/Ejercicio1.js
@@ -0,0 +1,15 @@
+var numero1 = 5;
+var numero2 = 8;
+
+if(...) {
+  alert("numero1 no es mayor que numero2");
+}
+if(...) {
+  alert("numero2 es positivo")
+}
+if(...) {
+  alert("numero1 es negativo o distinto de cero");
+}
+if(...) {
+  alert("incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
