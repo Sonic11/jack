46efe0b566d8383cab3117314517c324c9feba37
Arturo <arturo@iMac-de-labdis12.local>
2014-04-01 14:05:09 -0600

    ejercicio 3

---
+++ assets/js/curso_3_A.js
@@ -0,0 +1,16 @@
+
+var meses_array = ['Enero', 'Ebrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+alert(meses_array[0])
+alert(meses_array[1])
+alert(meses_array[2])
+alert(meses_array[3])
+alert(meses_array[4])
+alert(meses_array[5])
+alert(meses_array[6])
+alert(meses_array[7])
+alert(meses_array[8])
+alert(meses_array[9])
+alert(meses_array[10])
+alert(meses_array[11])
+
